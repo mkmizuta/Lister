4ad3f37aa3d185374d84aaa45ee0d88008d8e9a6
Bookis Smuin <bookis.smuin@gmail.com>
2014-01-30 14:33:13 -0800

    adds rescue to make controllers more clear

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,66 +1,73 @@
 require 'spec_helper'
-
-describe "SessionController" do
-  before { @controller = SessionController if defined? SessionController}
-  describe "GET 'new'" do
-    
-    it "is successful" do
-      get :new
-      expect(response).to be_successful
-    end
-    
-  end
+begin
+  describe SessionController do
   
-  describe "POST 'create'" do
+    describe "GET 'new'" do
     
-    context "when an existing user submits" do
-      let(:user) { create(:user) }
-      context "valid attributes" do
-        it "redirects" do
-          post :create, username: user.username, password: user.password
-          expect(response.status).to eq 302
-        end
+      it "is successful" do
+        get :new
+        expect(response).to be_successful
+      end
+    
+    end
+  
+    describe "POST 'create'" do
+    
+      context "when an existing user submits" do
+        let(:user) { create(:user) }
+        context "valid attributes" do
+          it "redirects" do
+            post :create, username: user.username, password: user.password
+            expect(response.status).to eq 302
+          end
       
-        it "sets the session[:user_id] to the users id" do
-          post :create, username: user.username, password: user.password
-          expect(session[:user_id]).to eq user.id
-        end
+          it "sets the session[:user_id] to the users id" do
+            post :create, username: user.username, password: user.password
+            expect(session[:user_id]).to eq user.id
+          end
         
-        it "sets a flash message" do
-          post :create, username: user.username, password: user.password
-          expect(flash[:notice]).to eq 'Successfully signed in'
+          it "sets a flash message" do
+            post :create, username: user.username, password: user.password
+            expect(flash[:notice]).to eq 'Successfully signed in'
+          end
+        end
+    
+        context "the wrong password" do
+          it "render the new template" do
+            post :create, username: user.username, password: "wrong"
+            expect(response).to render_template :new
+          end
+      
+          it "don't set the session[:user_id] to the users id" do
+            post :create, username: user.username, password: "wrong"
+            expect(session[:user_id]).to be_nil
+          end
+        
+          it "sets a flash message" do
+            post :create, username: user.username, password: "wrong"
+            expect(flash[:notice]).to eq "Invalid username or password"
+          end
         end
       end
     
-      context "the wrong password" do
-        it "render the new template" do
-          post :create, username: user.username, password: "wrong"
+      context "when a user doesn't exist yet" do
+        it "renders the new template" do
+          post :create, username: "I don't exist", password: "somepass"
           expect(response).to render_template :new
         end
       
-        it "don't set the session[:user_id] to the users id" do
-          post :create, username: user.username, password: "wrong"
-          expect(session[:user_id]).to be_nil
-        end
-        
         it "sets a flash message" do
-          post :create, username: user.username, password: "wrong"
+          post :create, username: "I don't exist", password: "wrong"
           expect(flash[:notice]).to eq "Invalid username or password"
         end
+      
       end
     end
-    
-    context "when a user doesn't exist yet" do
-      it "renders the new template" do
-        post :create, username: "I don't exist", password: "somepass"
-        expect(response).to render_template :new
-      end
-      
-      it "sets a flash message" do
-        post :create, username: "I don't exist", password: "wrong"
-        expect(flash[:notice]).to eq "Invalid username or password"
-      end
-      
-    end
+  end
+rescue
+  describe "SessionController" do
+   it "has a SessionController" do
+     expect(nil).to eq "SessionController"
+   end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,50 +1,56 @@
 require 'spec_helper'
+begin
+  describe UsersController do
 
-describe "UsersController" do
-  before { @controller = UsersController if defined? UsersController}
-
-  describe "GET 'new'" do
-    it "is successful" do
-      get :new
-      expect(response).to be_successful
+    describe "GET 'new'" do
+      it "is successful" do
+        get :new
+        expect(response).to be_successful
+      end
     end
-  end
   
-  describe "POST 'create'" do
-    context "with valid attributes" do
-      let(:valid_attributes) { {username: "bookis", email: "b@c.com", password: "gogo1234", password_confirmation: "gogo1234"} }
-      it "is a redirect" do
-        post :create, user: valid_attributes
-        expect(response.status).to eq 302 # This is a redirect
-      end
+    describe "POST 'create'" do
+      context "with valid attributes" do
+        let(:valid_attributes) { {username: "bookis", email: "b@c.com", password: "gogo1234", password_confirmation: "gogo1234"} }
+        it "is a redirect" do
+          post :create, user: valid_attributes
+          expect(response.status).to eq 302 # This is a redirect
+        end
       
-      it "changes user count by 1" do
-        expect { post :create, user: valid_attributes }.to change(User, :count).by(1)
-      end
+        it "changes user count by 1" do
+          expect { post :create, user: valid_attributes }.to change(User, :count).by(1)
+        end
       
-      it "sets a flash message" do
-        post :create, user: valid_attributes
-        expect(flash[:notice]).to_not be_blank
+        it "sets a flash message" do
+          post :create, user: valid_attributes
+          expect(flash[:notice]).to_not be_blank
+        end
       end
-    end
     
-    context "with invalid attributes" do
-      it "renders the new template" do
-        post :create, user: {username: "b"}
-        expect(response).to render_template :new
-      end
+      context "with invalid attributes" do
+        it "renders the new template" do
+          post :create, user: {username: "b"}
+          expect(response).to render_template :new
+        end
       
-      it "does not create a user" do
-        expect { post :create, user: {username: "b"} }.to change(User, :count).by(0)
+        it "does not create a user" do
+          expect { post :create, user: {username: "b"} }.to change(User, :count).by(0)
+        end
+      end
+    end
+  
+    describe "GET 'show'" do
+      let(:user) { create(:user) }
+      it "is successful" do
+        get :show, id: user.id
+        expect(response).to be_successful
       end
     end
   end
-  
-  describe "GET 'show'" do
-    let(:user) { create(:user) }
-    it "is successful" do
-      get :show, id: user.id
-      expect(response).to be_successful
-    end
+rescue
+  describe "UsersController" do
+   it "has a UsersController" do
+     expect(nil).to eq "UsersController"
+   end
   end
 end
